c24d4cacebed76868064ea6aca0e954da69b24b2
Christian Rojas <cjrojas72@gmail.com>
2020-06-18 23:34:37 -0400

    init commit

---
+++ api/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=50)
+    website = models.CharField(max_length=50)
+
+
+class ShoeType(models.Model):
+    style = models.CharField(max_length=50)
+
+
+class ShoeColor(models.Model):
+    COLORS_CHOICES = [
+        ("RD", "RED"),
+        ("OR", "ORANGE"),
+        ("YL", "YELLOW"),
+        ("GR", "GREEN"),
+        ("BL", "BLUE"),
+        ("IN", "INDIGO"),
+        ("VL", "VIOLET"),
+        ("WH", "WHITE"),
+        ("BK", "BLACK")
+    ]
+
+    color = models.CharField(
+        max_length=2,
+        choices=COLORS_CHOICES,
+        default="WH"
+    )
+
+
+class Shoe(models.Model):
+    size = models.IntegerField()
+    brand_name = models.CharField(max_length=50)
+    manufacturer = models.ForeignKey(
+        Manufacturer, related_name="manufacturer", on_delete=models.CASCADE)
+    colors = models.ForeignKey(
+        ShoeColor, related_name="colors", on_delete=models.CASCADE)
+    shoe_type = models.ForeignKey(
+        ShoeType, related_name="type", on_delete=models.CASCADE)
+    fasten_type = models.CharField(max_length=50)
